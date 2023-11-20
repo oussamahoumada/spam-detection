c8faa122d50b365e08ee6fd9f1773e67d9e3425c
houmada <oussamahoumada4@gmail.com>
2023-11-20 09:37:48 +0100

    add params

--- authParams.js
+++ authParams.js
@@ -1,6 +1,6 @@
 export const params = {
-  api: "",
-  key: "",
-  ident: "",
-  mail: "",
+  api: "http://localhost/Paris8/Master2/omeka-s/api/",
+  key: "eDA7lHzv65DZudsvAO3b4KhyYhrTn0SL",
+  ident: "SRSzlkuGkcLCePs7CF8AwnRPNt7FvsFR",
+  mail: "oussamahoumada5@gmail.com",
 };
