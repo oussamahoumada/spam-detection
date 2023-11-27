d39b4e7ccc13314a7c736074677a4dd720f8cbe4
houmada <oussamahoumada4@gmail.com>
2023-11-27 14:39:43 +0100

    update authParams

--- authParams.js
+++ authParams.js
@@ -1,5 +1,5 @@
 export const params = {
-  api: "http://localhost/Paris8/Master2/omeka-s/api/",
+  api: "http://localhost/omk_thyp_23-24_clone/api/",
   key: "eDA7lHzv65DZudsvAO3b4KhyYhrTn0SL",
   ident: "SRSzlkuGkcLCePs7CF8AwnRPNt7FvsFR",
   mail: "oussamahoumada5@gmail.com",
